1a4bcae261c2376c427a49bbf092bb786bccc83d
Ashwyeth Sunil <ashweyth.sunil@asint.net>
2022-02-10 13:53:37 +0530

    Task 4 Add
    An attempt to build a http cache

---
+++ task-4/src/main/java/com/asint/task4/Task4Application.java
@@ -0,0 +1,13 @@
+package com.asint.task4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Task4Application.class, args);
+	}
+
+}
